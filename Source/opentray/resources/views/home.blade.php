f84dc73cb851ba4704c16ef0c3027b5656111d05
Jon Kelly <jonathan.r.kelly@wmich.edu>
2019-12-11 07:18:28 -0500

    Changed the homepage

--- Source/opentray/resources/views/home.blade.php
+++ Source/opentray/resources/views/home.blade.php
@@ -1,49 +1,32 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-
-<style>
-    html,
-    body,
-    .container,
-    .row {
-        height: 100%;
-    }
-
-    .col-6 {
-        background-color: grey;
-        border-style: solid;
-        border-color: red;
-        align-items: center;
-        justify-content: center;
-        display: flex;
-    }
-</style>
-
+<!DOCTYPE html>
+<html>
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Home</title>
-    <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
-    <script src="{{ asset('js/bootstrap.min.js') }}"></script>
+	<title>Metron Dietary</title>
+	<meta charset="utf-8" name="viewport" content="width=device-width, initial-scale=1.0">
+	<link href="{{ asset('css/dietary.css') }}" rel="stylesheet">
+	<script type="text/javascript" src="{{ asset('cjs/dietary.js') }}"></script>
 </head>
-
 <body>
-    <div class="container text-center" dusk="test">
-        <div class="row">
-            <div class="col-6">
-                <img src="images/test.png">
-            </div>
-            <div class="col-6">
-                <img src="images/test.png">
-            </div>
-            <div class="col-6">
-                <img src="images/test.png">
-            </div>
-            <div class="col-6">
-                <img src="images/test.png">
-            </div>
-        </div>
-    </div>
-</body>
+	<div id="mainContainer">
+		<div id="header">
+			<p class="header_button">Login</p>
+		</div>
+		<div id="mainw_window">
+			<div class="link_container">
+				<img src="images/img_avatar2.png" alt="Avatar" class="avatar">
+				<label>Residents</label>
+			</div>
+			<div class="link_container">
+				<img src="images/Chef.png" alt="Avatar" class="avatar">
+				<label>Tray Cards</label>
+			</div>
+			<div class="link_container">
+				<img src="images/Avatar-chef-cook-service-512.png" alt="Avatar" class="avatar">
+				<label>Tasks</label>
+			</div>
+		</div>
+	</div>
 
+</body>
 </html>
