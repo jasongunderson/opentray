82d6df75e7737c0507e910ae9ee02f3d02ca5891
Jon Kelly <jonathan.r.kelly@wmich.edu>
2020-02-01 10:23:20 -0500

    sql

--- Documents/sql.sql
+++ Documents/sql.sql
@@ -1,23 +1,30 @@
 CREATE DATABASE opentray;
 USE opentray;
 
-CREATE TABLE `opentray`.`residents` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `facility` VARCHAR(45) NOT NULL,
-  `fname` VARCHAR(45) NOT NULL,
-  `lname` VARCHAR(45) NOT NULL,
-  `room` INT NULL,
-  `likes` TINYTEXT NULL,
-  `dislikes` TINYTEXT NULL,
-  `allergies` TINYTEXT NULL,
-  `active` BIT NULL DEFAULT 1,
-  PRIMARY KEY (`id`, `facility`));
+CREATE TABLE opentray.residents (
+  id INT NOT NULL AUTO_INCREMENT,
+  facility INT NULL,
+  fname TEXT NOT NULL,
+  lname TEXT NOT NULL,
+  room INT NULL,
+  dine TEXT NULL,
+  likes TEXT NULL,
+  dislikes TEXT NULL,
+  allergies TEXT NULL,
+  comment TEXT NULL,
+  active BIT NOT NULL DEFAULT 1,
+  PRIMARY KEY (id));
 
-  CREATE TABLE `opentray`.`staff` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `facility` VARCHAR(45) NOT NULL,
-  `fname` VARCHAR(45) NOT NULL,
-  `lname` VARCHAR(45) NOT NULL,
-  `permission` INT NOT NULL,
-  `active` BIT NULL DEFAULT 1,
-  PRIMARY KEY (`id`, `facility`));
+CREATE TABLE opentray.staff (
+  id INT NOT NULL AUTO_INCREMENT,
+  facility INT NULL,
+  fname TEXT NOT NULL,
+  lname TEXT NOT NULL,
+  permission INT NOT NULL,
+  active BIT NULL DEFAULT 1,
+PRIMARY KEY (id));
+
+CREATE TABLE opentray.facilities (
+  id INT NOT NULL AUTO_INCREMENT,
+  facility TEXT NOT NULL,
+PRIMARY KEY (id));
